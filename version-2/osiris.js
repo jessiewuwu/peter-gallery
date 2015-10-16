c4b8fcfa65598d2b3736e4346ea36b83b9696fa2
jessiewuwu <jwu004@gmail.com>
2015-10-15 18:37:23 -0700

    modal works on click

--- version-2/osiris.js
+++ version-2/osiris.js
@@ -1,21 +1,22 @@
 $( document ).ready(function() {
 
-$('.panel').hover(function(){
-  var img_id = $(this).attr("data-id");
-  $('.big-wrap').attr("id", img_id);
-})
+  $('.panel').hover(function(){
+    var img_id = $(this).attr("data-id");
+    $('.big-wrap').attr("id", img_id);
+  })
 
 
-
-$('.thumbnail').click(function(){
-  $('.modal-body').empty();
-  var title = $(this).parent('a').attr("title");
-  $('.modal-title').html(title);
-  $($(this).parents('div').html()).appendTo('.modal-body');
-  // jQuery.noConflict();
-  $('#myModal').modal({show:true});
-});
+  $('.thumbnail').click(function(){
+      $('.modal-body').empty();
+      var title = $(this).parent('a').attr("title");
+      $('.modal-title').html(title);
+      $($(this).parents('div').html()).appendTo('.modal-body');
+      $('#myModal').modal({show:true});
+  });
 
 });
+
+
+
 
 
